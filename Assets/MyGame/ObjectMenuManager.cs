30ed47602145b2db752fde204e58e8f06be5b4f6
smeerws <hello@smeerws.at>
2019-05-04 12:45:58 +0200

    started menu system, added player prefab

---
+++ Assets/MyGame/ObjectMenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMenuManager : MonoBehaviour
+{
+    public List<GameObject> objectList;
+    public List<GameObject> objectPrefabList;
+    public int currentObjectIndex = 0;
+
+	void Start ()
+    {
+        foreach (Transform child in transform)
+        {
+            objectList.Add (child.gameObject);
+        }
+	}
+
+    public void MenuLeft ()
+    {
+        objectList[currentObjectIndex].SetActive (false);
+        currentObjectIndex--;
+
+        if (currentObjectIndex < 0)
+        {
+            currentObjectIndex = objectList.Count - 1;
+        }
+
+        objectList[currentObjectIndex].SetActive (true);
+    }
+
+    public void MenuRight ()
+    {
+        objectList[currentObjectIndex].SetActive (false);
+        currentObjectIndex++;
+
+        if (currentObjectIndex >= objectList.Count)
+        {
+            currentObjectIndex = 0;
+        }
+
+        objectList[currentObjectIndex].SetActive (true);
+    }
+}
