08ea138e2e36036f42b9b0db54e8ad0054be75e3
smeerws <hello@smeerws.at>
2019-05-18 09:55:40 +0200

    button scripts implemented, interaction with buttons not working

--- Assets/MyGame/Scripts/ButtonManager.cs
+++ Assets/MyGame/Scripts/ButtonManager.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR;
 
 public class ButtonManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void SwitchToWelcomeScene()
+    {
+        SteamVR_LoadLevel.Begin("01WelcomeOverview", false, 2.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    public void SwitchToTutorialScene()
+    {
+        SteamVR_LoadLevel.Begin("02Tutorial", false, 2.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+    }
 }
