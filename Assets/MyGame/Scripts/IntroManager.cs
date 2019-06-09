97c7b1c18d86eda1a4345d212cef2cfc9a4f41f8
smeerws <hello@smeerws.at>
2019-06-09 12:44:27 +0200

    remove laserpointerbeam mechanic when switching the scene

---
+++ Assets/MyGame/Scripts/IntroManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class IntroManager : MonoBehaviour
+{
+    public LaserPointerManager buttonManager;
+
+    public SteamVR_Action_Boolean grapPinch;
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
+    
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collided object other " + other.name);
+        Debug.Log(grapPinch.state);
+
+        if (gameObject.name == "BtnTutorial" && grapPinch.stateDown)
+        {
+            gameObject.SetActive(false);
+            
+            SteamVR_LoadLevel.Begin("02Tutorial", false, 2.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+            Debug.Log("Switch Scene");
+        }
+    }
+
+}
---
+++ Assets/MyGame/Scripts/LaserPointerManager.cs
@@ -0,0 +1,46 @@
+﻿//LaserPointer script is added onto LeftHand and Righthand for pointing in introScence
+//When switching into the next scene, the laserpointer still remains because the player is in don't destroy on load
+//On Destroy this script destroys the SteamVR_Laserpointer component as well as the cube object made by the script 
+//for the pointer beam before switching the scene. 
+
+using UnityEngine;
+using Valve.VR.Extras;
+
+public class LaserPointerManager : MonoBehaviour
+{
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    private SteamVR_LaserPointer laserPointerLeft;
+    private SteamVR_LaserPointer laserPointerRight;
+
+    private void Start()
+    {
+        laserPointerLeft = leftHand.GetComponent<SteamVR_LaserPointer>();
+        laserPointerRight = rightHand.GetComponent<SteamVR_LaserPointer>();
+    }
+
+    //Destroy all laserObjects made by laserPointer in the next scene
+    private void DestroyLaserBeam(SteamVR_LaserPointer laserPointer)
+    {
+        Transform[] tmpObjects = laserPointer.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < tmpObjects.Length; i++)
+        {
+            if (tmpObjects[i].name.StartsWith("New"))
+            {
+                Destroy(tmpObjects[i].gameObject);
+                break;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        DestroyLaserBeam(laserPointerLeft);
+        DestroyLaserBeam(laserPointerRight);
+
+        Destroy(laserPointerLeft);
+        Destroy(laserPointerRight);
+    }
+}
