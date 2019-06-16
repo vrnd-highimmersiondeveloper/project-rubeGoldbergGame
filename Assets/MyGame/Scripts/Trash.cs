97741a3832392aaf26f8c344f25a4a3df7d0e65d
smeerws <hello@smeerws.at>
2019-06-16 19:43:43 +0200

    added collider to trash

--- Assets/MyGame/Scripts/Trash.cs
+++ Assets/MyGame/Scripts/Trash.cs
@@ -4,7 +4,23 @@ public class Trash : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
+        Debug.Log("collider object other name " + other.gameObject.name);
+        if (other.gameObject.name == "Fan_Body")
+        {
+            Destroy(other.GetComponent<Transform>().parent.gameObject);
+        }
+        else if (other.gameObject.name.Contains("Wood_Plank"))
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.name == "GravityZoneCylinder")
+        {
+            Destroy(other.GetComponent<Transform>().parent.gameObject);
+        }
+        else if (other.gameObject.name == "TrampolineJumpArea")
+        {
+            Destroy(other.GetComponent<Transform>().parent.gameObject);
+        }
     }
 
 }
